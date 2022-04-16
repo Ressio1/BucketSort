16ecbad9796a35cdcba77706f7185c13710301db
Baptiste <bapti89@free.fr>
2022-04-16 10:57:22 +0200

    Commit #5 : ajout d’un test sur une liste à 3 éléments non triés

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,24 +5,49 @@ public class Main {
     public static double[] sort(double[] input) {
         var maxOpt = Arrays.stream(input).max();
         if (maxOpt.isEmpty()) {
-            return input;
+            return input; //Si l'entree est vide on retourne l'entree.
         }
 
         var maximum = maxOpt.getAsDouble();
-        var n = input.length; //Calcul taille
+        var n = input.length; //Calcule taille
         var buckets1 = new double[10][n]; //Bucket
-        var buckets2 = new int[10];
-        Arrays.fill(buckets2, 0); //Remplissage du bucket avec des 0
+        var resultatFinal = new double[n];
+        var Marker = new int[10];
+        Arrays.fill(Marker, 0); //Remplissage du bucket avec des 0.
 
+        //
         for (int i = 0; i < input.length; i++) {
             var j = (Integer) Math.round((float) (input[i] / ((maximum + 1) / n)));
-            buckets1[j][buckets2[j]] = input[i];
-            buckets2[j] += 1;
+            buckets1[j][Marker[j]] = input[i];
+            Marker[j] += 1;
         }
 
-        return input;
+        var resultCounter = 0;
+        for (int i = 0; i < buckets1.length; i++) {
+            Sort2(buckets1[i], Marker[i]);
+            for (int j = 0; j < Marker[i]; j++) {
+                if (resultCounter < resultatFinal.length) {
+                    resultatFinal[resultCounter++] = buckets1[i][j];
+                }
+            }
+        }
+        return resultatFinal; //Retourne le resultat final.
     }
-
+    //Tri des buckets
+    private static void Sort2(double[] array, int nbItems) {
+        var Trifini = false;
+        while (Trifini == false) {
+            Trifini = true;
+            for (int i = 0; i < nbItems - 1; i++) {
+                if (array[i] > array[i + 1]) {
+                    var tmp = array[i + 1];
+                    array[i + 1] = array[i];
+                    array[i] = tmp;
+                    Trifini = false;
+                }
+            }
+        }
+    }
 
 }
 
--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -31,5 +31,17 @@ public class MainTest {
         Assertions.assertArrayEquals(expected, Main.sort(input));
     }
 
+    @Test
+    public void testSort2ItemsInARowNotSorted() {
+        var expected = new double[]{1.7f, 1.9f};
+        var input = new double[]{1.9f, 1.7f};
+        Assertions.assertArrayEquals(expected, Main.sort(input));
+    }
 
+    @Test
+    public void testSort7Items() {
+        var expected = new double[]{1.7f, 1.9f, 2.1f, 2.2f, 2.3f, 4.3f, 8.1f};
+        var input = new double[]{1.9f, 1.7f, 4.3f, 2.3f, 2.2f, 2.1f, 8.1f};
+        Assertions.assertArrayEquals(expected, Main.sort(input));
+    }
 }
